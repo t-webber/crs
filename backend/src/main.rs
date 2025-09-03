b7c33d6411d0e633433a70db6b52d4357dcff4a6
Tom Webber <t.c.w.webber@gmail.com>
2025-09-03 20:54:24 +0200

    feat(backend): send messages

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,20 @@
-use matrix_sdk::{Client, config::SyncSettings, ruma::events::room::message::SyncRoomMessageEvent};
+use std::{thread, time::Duration};
+
+use matrix_sdk::{
+    Client,
+    config::SyncSettings,
+    ruma::{
+        TransactionId,
+        events::room::message::{RoomMessageEventContent, SyncRoomMessageEvent},
+    },
+};
+
+async fn sync_client(client: Client) {
+    client
+        .sync(SyncSettings::default())
+        .await
+        .unwrap_or_else(|err| panic!("Failed to synchronise with the server: {err:?}"));
+}
 
 #[tokio::main]
 async fn main() {
@@ -10,8 +26,6 @@ async fn main() {
         .await
         .unwrap_or_else(|err| panic!("Failed to build client: {err:?}"));
 
-    println!("Built client");
-
     client
         .matrix_auth()
         .login_username("@c:localhost", "c")
@@ -19,21 +33,31 @@ async fn main() {
         .await
         .unwrap_or_else(|err| panic!("Failed to login: {err:?}"));
 
-    println!("User connected!");
-
-    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+    client.add_event_handler(async move |ev: SyncRoomMessageEvent| {
         println!(
-            "[{:?}] New message from {}: {}",
+            "\x1b[35m[{:?}] New message from {}: {}\x1b[0m",
             ev.origin_server_ts(),
             ev.sender(),
             ev.as_original().unwrap().content.body(),
         );
     });
 
-    client
-        .sync(SyncSettings::default())
-        .await
-        .unwrap_or_else(|err| panic!("Failed to synchronise with the server: {err:?}"));
+    let synchronisation_handler = tokio::spawn(sync_client(client.clone()));
 
+    let mut backoff = 1;
+    loop {
+        thread::sleep(Duration::from_secs(backoff));
+        backoff <<= 1;
+        if let Some(room) = client.rooms().first() {
+            room.send(RoomMessageEventContent::text_plain("Hello from Rust"))
+                .with_transaction_id(TransactionId::new())
+                .await
+                .unwrap();
+            println!("\x1b[35mMessage sent!\x1b[0m");
+            break;
+        }
+    }
+
+    synchronisation_handler.await.unwrap();
     unreachable!("Sync function never returns")
 }
