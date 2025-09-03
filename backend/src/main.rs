8f72744c1809e20ae02c2503bf64ddbaab3de00e
Tom Webber <t.c.w.webber@gmail.com>
2025-09-03 19:48:56 +0200

    feat(backend): listen for incoming messages

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use matrix_sdk::Client;
+use matrix_sdk::{Client, config::SyncSettings, ruma::events::room::message::SyncRoomMessageEvent};
 
 #[tokio::main]
 async fn main() {
@@ -8,13 +8,30 @@ async fn main() {
         .await
         .unwrap_or_else(|err| panic!("Failed to build client: {err:?}"));
 
+    println!("Built client");
+
     client
         .matrix_auth()
-        .login_username("@b:localhost", "b")
+        .login_username("@c:localhost", "c")
         .send()
         .await
         .unwrap_or_else(|err| panic!("Failed to login: {err:?}"));
 
-    println!("Successfully logged in");
-    dbg!(client);
+    println!("User connected!");
+
+    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+        println!(
+            "[{:?}] New message from {}: {}",
+            ev.origin_server_ts(),
+            ev.sender(),
+            ev.as_original().unwrap().content.body(),
+        );
+    });
+
+    client
+        .sync(SyncSettings::default())
+        .await
+        .unwrap_or_else(|err| panic!("Failed to synchronise with the server: {err:?}"));
+
+    unreachable!("Sync function never returns")
 }
