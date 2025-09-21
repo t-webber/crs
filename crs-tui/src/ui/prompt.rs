585b253d88150f8d607169489ffd0e6023cc3966
Tom Webber <t.c.w.webber@gmail.com>
2025-09-21 15:17:24 +0200

    feat: display items that match search in the prompt component

--- crs-tui/src/ui/prompt.rs
+++ crs-tui/src/ui/prompt.rs
@@ -1,13 +1,14 @@
 //! Component that displays an input at the middle of the page
 
 use core::convert::Infallible;
+use core::fmt::Display;
 
 use ratatui::Frame;
 use ratatui::crossterm::event::Event;
 use ratatui::layout::{Alignment, Constraint, Direction, Layout, Rect};
 use ratatui::style::{Color, Style};
-use ratatui::text::Text;
-use ratatui::widgets::Block;
+use ratatui::text::{Line, Text};
+use ratatui::widgets::{Block, Paragraph};
 
 use crate::ui::component::Component;
 use crate::ui::input::Input;
@@ -17,52 +18,91 @@ use crate::ui::widgets::{grid_center, saturating_cast};
 pub struct PromptSubmit<T>(pub T);
 
 /// Popup to create a room
-pub struct Prompt<T> {
+pub struct Prompt<T: Display> {
+    /// List of possible responses
+    entries: Option<Vec<T>>,
     /// Input to enter the name of the room to be create
-    input:            Input<'static>,
+    input:   Input<'static>,
     /// Message to display in the prompt, including error messages and loading
     /// statuses
-    message:          Status,
-    /// List of possible responses
-    possible_entries: Option<Vec<T>>,
+    message: Status,
     /// Title of the prompt
-    title:            &'static str,
+    title:   &'static str,
 }
 
-impl<T> Prompt<T> {
+impl<T: Display> Prompt<T> {
+    /// Draws the border around the prompt
+    fn draw_border(&self, frame: &mut Frame<'_>, popup_area: Rect) {
+        let block = Block::bordered()
+            .title(self.title)
+            .title_alignment(Alignment::Center)
+            .title_style(Style::new().fg(Color::Yellow));
+        frame.render_widget(block, popup_area);
+    }
+
+    /// Returns the first possible entries that match the search
+    fn get_possibilites(&self, max_number: usize) -> Option<Vec<String>> {
+        let value = self.input.as_value();
+        let entries = self.entries.as_ref()?;
+        Some(
+            entries
+                .iter()
+                .filter_map(|entry| {
+                    let formatted = format!("{entry}");
+                    formatted.contains(value).then_some(formatted)
+                })
+                .take(max_number)
+                .collect(),
+        )
+    }
+
     /// Create [`CreateRoom`] component
     pub const fn new(input: Input<'static>, title: &'static str) -> Self {
-        Self { input, title, message: Status::None, possible_entries: None }
+        Self { input, title, message: Status::None, entries: None }
     }
 
     /// Create [`CreateRoom`] component
     pub const fn new_with_list(
         input: Input<'static>,
         title: &'static str,
-        possible_entries: Vec<T>,
+        entries: Vec<T>,
     ) -> Self {
-        Self {
-            input,
-            title,
-            message: Status::None,
-            possible_entries: Some(possible_entries),
-        }
+        Self { input, title, message: Status::None, entries: Some(entries) }
     }
 
-    /// Render the prompt on the screen
+    /// Updates the status of the prompt, to display loading and error
+    /// information
+    fn update_status(&mut self, status: Status) {
+        self.message = status;
+        self.input.set_active(!matches!(self.message, Status::Submitting));
+    }
+}
+
+impl<T: Display> Component for Prompt<T> {
+    type ResponseData = Status;
+    type UpdateState = T;
+
     #[expect(clippy::arithmetic_side_effects, reason = "width >= 20")]
-    fn draw_prompt(&self, frame: &mut Frame<'_>, area: Rect) {
+    fn draw(&self, frame: &mut Frame<'_>, area: Rect) {
         let width = (area.width - 2).min(50);
 
-        let error_height = self.message.as_content().map_or(0, |error| {
-            saturating_cast(error.0.len())
-                .saturating_div(width)
-                .saturating_add(1)
+        let message = self.message.as_content();
+        let message_height = message.map_or(0, |msg| {
+            saturating_cast(msg.0.len()).saturating_div(width).saturating_add(1)
         });
 
         let input_height = self.input.height();
 
-        let height = input_height + 4 + error_height;
+        let mut height = input_height + 4 + message_height;
+
+        let max_possibilities_nb = area.height.saturating_sub(height);
+        let possibilities = self.get_possibilites(max_possibilities_nb.into());
+
+        let possibilities_height = possibilities
+            .as_ref()
+            .map_or(0, |possib| saturating_cast(possib.len()));
+
+        height += possibilities_height;
 
         let popup_area = grid_center(
             Constraint::Length(width),
@@ -70,47 +110,27 @@ impl<T> Prompt<T> {
             area,
         );
 
-        let block = Block::bordered()
-            .title(self.title)
-            .title_alignment(Alignment::Center)
-            .title_style(Style::new().fg(Color::Yellow));
-        frame.render_widget(block, popup_area);
+        self.draw_border(frame, popup_area);
 
         let layout = Layout::new(Direction::Vertical, [
             Constraint::Length(input_height),
-            Constraint::Length(error_height),
+            Constraint::Length(message_height),
+            Constraint::Length(possibilities_height),
         ])
         .margin(2)
         .split(popup_area);
 
         self.input.draw(frame, layout[0]);
 
-        if let Some((error_message, colour)) = self.message.as_content() {
-            let error_component = Text::from(error_message)
-                .style(Style::new().fg(colour))
-                .centered();
-            frame.render_widget(error_component, layout[1]);
+        if let Some((content, colour)) = message {
+            let component =
+                Text::from(content).style(Style::new().fg(colour)).centered();
+            frame.render_widget(component, layout[1]);
         }
-    }
 
-    /// Updates the status of the prompt, to display loading and error
-    /// information
-    fn update_status(&mut self, status: Status) {
-        self.message = status;
-        if matches!(self.message, Status::Submitting) {
-            self.input.set_active(false);
-        } else {
-            self.input.set_active(true);
+        if let Some(possible_entries) = possibilities {
+            draw_possibilities(frame, layout[2], &possible_entries);
         }
-    }
-}
-
-impl<T> Component for Prompt<T> {
-    type ResponseData = Status;
-    type UpdateState = T;
-
-    fn draw(&self, frame: &mut Frame<'_>, area: Rect) {
-        self.draw_prompt(frame, area);
     }
 
     async fn on_event(&mut self, event: Event) -> Option<Self::UpdateState> {
@@ -149,4 +169,25 @@ impl Status {
             Self::Submitting => Some(("Submitting...", Color::Green)),
         }
     }
+}
+
+/// Draws the list of matching entries
+fn draw_possibilities(
+    frame: &mut Frame<'_>,
+    area: Rect,
+    possible_entries: &[String],
+) {
+    if possible_entries.is_empty() {
+        let empty = Text::from("No corresponding entry")
+            .style(Style::new().fg(Color::Red))
+            .centered();
+        frame.render_widget(empty, area);
+    }
+
+    let lines = possible_entries
+        .iter()
+        .map(|entry| Line::from(entry.as_str()))
+        .collect::<Vec<_>>();
+
+    frame.render_widget(Paragraph::new(lines), area);
 }
