3ec2b339758dc409668fab0957e2727b1c630603
Tom Webber <t.c.w.webber@gmail.com>
2025-09-21 08:24:41 +0200

    feat: add prompt's core logic

--- crs-tui/src/ui/prompt.rs
+++ crs-tui/src/ui/prompt.rs
@@ -14,32 +14,44 @@ use crate::ui::input::Input;
 use crate::ui::widgets::{grid_center, saturating_cast};
 
 /// Struct to send the data after submission
-pub struct PromptSubmit(pub String);
+pub struct PromptSubmit<T>(pub T);
 
 /// Popup to create a room
-pub struct Prompt {
+pub struct Prompt<T> {
     /// Input to enter the name of the room to be create
-    input:   Input<'static>,
+    input:            Input<'static>,
     /// Message to display in the prompt, including error messages and loading
     /// statuses
-    message: Status,
+    message:          Status,
+    /// List of possible responses
+    possible_entries: Option<Vec<T>>,
     /// Title of the prompt
-    title:   &'static str,
+    title:            &'static str,
 }
 
-impl Prompt {
+impl<T> Prompt<T> {
     /// Create [`CreateRoom`] component
     pub const fn new(input: Input<'static>, title: &'static str) -> Self {
-        Self { input, title, message: Status::None }
+        Self { input, title, message: Status::None, possible_entries: None }
     }
-}
 
-impl Component for Prompt {
-    type ResponseData = Status;
-    type UpdateState = PromptSubmit;
+    /// Create [`CreateRoom`] component
+    pub const fn new_with_list(
+        input: Input<'static>,
+        title: &'static str,
+        possible_entries: Vec<T>,
+    ) -> Self {
+        Self {
+            input,
+            title,
+            message: Status::None,
+            possible_entries: Some(possible_entries),
+        }
+    }
 
+    /// Render the prompt on the screen
     #[expect(clippy::arithmetic_side_effects, reason = "width >= 20")]
-    fn draw(&self, frame: &mut Frame<'_>, area: Rect) {
+    fn draw_prompt(&self, frame: &mut Frame<'_>, area: Rect) {
         let width = (area.width - 2).min(50);
 
         let error_height = self.message.as_content().map_or(0, |error| {
@@ -81,23 +93,39 @@ impl Component for Prompt {
         }
     }
 
-    async fn on_event(&mut self, event: Event) -> Option<Self::UpdateState> {
-        if let Some(key_event) = event.as_key_press_event()
-            && key_event.code.is_enter()
-        {
-            return Some(PromptSubmit(self.input.take_value()));
-        }
-        let _: Infallible = self.input.on_event(event).await?;
-        None
-    }
-
-    fn update(&mut self, response_data: Self::ResponseData) {
-        self.message = response_data;
+    /// Updates the status of the prompt, to display loading and error
+    /// information
+    fn update_status(&mut self, status: Status) {
+        self.message = status;
         if matches!(self.message, Status::Submitting) {
             self.input.set_active(false);
         } else {
             self.input.set_active(true);
         }
+    }
+}
+
+impl<T> Component for Prompt<T> {
+    type ResponseData = Status;
+    type UpdateState = T;
+
+    fn draw(&self, frame: &mut Frame<'_>, area: Rect) {
+        self.draw_prompt(frame, area);
+    }
+
+    async fn on_event(&mut self, event: Event) -> Option<Self::UpdateState> {
+        if let Some(key_event) = event.as_key_press_event()
+            && key_event.code.is_enter()
+        {
+            todo!()
+        } else {
+            let _: Infallible = self.input.on_event(event).await?;
+            None
+        }
+    }
+
+    fn update(&mut self, response_data: Self::ResponseData) {
+        self.update_status(response_data);
     }
 }
 
